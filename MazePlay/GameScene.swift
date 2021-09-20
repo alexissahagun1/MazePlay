40e192f55b557f440cb51d07e1b57df7964418e5
Rafael Jimenez <58578047+rafaeljimenez01@users.noreply.github.com>
2021-09-20 10:38:31 -0500

    Inciso 11

--- MazePlay/GameScene.swift
+++ MazePlay/GameScene.swift
@@ -8,6 +8,16 @@
 import SpriteKit
 import GameplayKit
 
+enum CollisionTypes: UInt32 {
+    case player = 1
+    case wall = 2
+    case star = 4
+    case hole = 8
+    case finish = 16
+    
+}
+
+
 class GameScene: SKScene {
     var player: SKSpriteNode!
     var lastTouchPosition: CGPoint?
@@ -23,6 +33,41 @@ class GameScene: SKScene {
         
         physicsWorld.gravity = .zero
         
+    }
+    
+    func loadLevel() {
+        guard let levelURL = Bundle.main.url(forResource: "maze1", withExtension: "txt") else { fatalError("Can't find maze1.txt in app bundle")}
+        
+        guard let levelString = try? String(contentsOf: levelURL) else { fatalError("Can't find maze1.txt in app bundle")}
+        
+        let lines = levelString.components(separatedBy: "\n")
+        
+        for(row,line) in lines.reversed().enumerated() {
+            for(column, letter) in line.enumerated() {
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                
+                if(letter == "w") {
+                    let node = SKSpriteNode(imageNamed: "block")
+                    node.position = position
+                    node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+                    node.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
+                    node.physicsBody?.isDynamic = false
+                    
+                    addChild(node)
+                } else if letter == "h" {
+                    
+                }
+                else if letter == "s" {
+                
+                }
+                else if letter == "f" {
+                
+                }
+                else if letter == " " {
+                
+                }
+            }
+        }
     }
 }
 
